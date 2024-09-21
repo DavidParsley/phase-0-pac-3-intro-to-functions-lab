a74a5682bce6d82f3859abf6e93d9993a222e653
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-09-21 12:23:53 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,23 @@
+// const { spyOn } = require("expect");
+
+function shout(string){
+    return string .toUpperCase("hello");
+}
+function whisper(string){
+    return string .toLowerCase('HELLO');
+}
+function logShout(string){
+    console.log('HELLO');   
+}
+function logWhisper(string){
+    console.log('hello');
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else {
+      return "I can't hear you!";
+    }
+  }
